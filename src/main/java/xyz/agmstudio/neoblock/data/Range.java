c84fe5c479692c801188cf7613d80d2bafdfea11
Ashenguard <myminecraft2019@gmail.com>
2025-03-15 14:26:00 +0330

    Added Solid Range

--- src/main/java/xyz/agmstudio/neoblock/data/Range.java
+++ src/main/java/xyz/agmstudio/neoblock/data/Range.java
@@ -12,12 +12,18 @@ public final class Range {
         return new Range(min, max);
     }
 
+    public Range(int solid) {
+        this.min = solid;
+        this.max = solid;
+    }
+
     public Range(int min, int max) {
         this.min = Math.min(min, max);
         this.max = Math.max(min, max);
     }
 
     public int get() {
+        if (min == max) return min;
         return RandomGenerator.getDefault().nextInt(min, max + 1);
     }
 
