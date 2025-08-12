1a82a02efbe6275b7023fc8812ddb81b9a083f1d
Ashenguard <myminecraft2019@gmail.com>
2025-08-12 18:58:52 +0330

    Reset project to basic for multiversion

---
+++ neoforge/src/main/java/xyz/agmstudio/neoblock/platform/NeoForgePlatformHelper.java
@@ -0,0 +1,26 @@
+package xyz.agmstudio.neoblock.platform;
+
+import xyz.agmstudio.neoblock.platform.services.IPlatformHelper;
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.loading.FMLLoader;
+
+public class NeoForgePlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+
+        return "NeoForge";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return ModList.get().isLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return !FMLLoader.isProduction();
+    }
+}
