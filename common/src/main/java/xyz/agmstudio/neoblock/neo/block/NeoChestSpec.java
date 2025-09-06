4757444e0e24d97062d9b8e257d92de5b9679bf1
Ashenguard <myminecraft2019@gmail.com>
2025-09-06 15:08:36 +0330

    [Common] Min & Max for chest loot

--- common/src/main/java/xyz/agmstudio/neoblock/neo/block/NeoChestSpec.java
+++ common/src/main/java/xyz/agmstudio/neoblock/neo/block/NeoChestSpec.java
@@ -19,11 +19,7 @@ import java.util.stream.IntStream;
 public class NeoChestSpec extends NeoBlockSpec {
     private static final Pattern PATTERN = Pattern.compile("^(?:(?<count>\\d+)x *)?neoblock:chest:(?<id>[^ ]+)$");
 
-    private static final HashMap<String, List<NeoItemSpec>> CHESTS = new HashMap<>();
-
-    public static List<NeoItemSpec> getChestItems(String name) {
-        return CHESTS.getOrDefault(name, List.of());
-    }
+    private static final HashMap<String, Holder> CHESTS = new HashMap<>();
 
     public static void reloadChests() {
         IConfig config = IConfig.getConfig(NeoBlock.CONFIG_FOLDER, "chests");
@@ -35,14 +31,17 @@ public class NeoChestSpec extends NeoBlockSpec {
         CHESTS.clear();
 
         config.forEach((key, value) -> {
-            List<String> entries = config.get(key, List.of());
+            IConfig section = config.getSection(key);
 
             List<NeoItemSpec> list = new ArrayList<>();
-            entries.forEach(entry -> NeoItemSpec.parseItem(entry).ifPresent(list::add));
+            List<String> items = section.get("items", List.of());
+            items.forEach(entry -> NeoItemSpec.parseItem(entry).ifPresent(list::add));
             if (list.isEmpty()) NeoBlock.LOGGER.info("Unable to load chest {} because it's empty.", key);
             else {
                 NeoBlock.LOGGER.info("Loaded {} stacks for {}", list.size(), key);
-                CHESTS.put(key, list);
+                int min = Math.max(0, section.getInt("min", 0));
+                int max = Math.min(27, section.getInt("max", 27));
+                CHESTS.put(key, new Holder(list, min, max));
             }
         });
 
@@ -53,21 +52,22 @@ public class NeoChestSpec extends NeoBlockSpec {
         Matcher matcher = PATTERN.matcher(input.trim());
         if (!matcher.matches()) return Optional.empty();
 
-        List<NeoItemSpec> items = CHESTS.getOrDefault(matcher.group("id"), List.of());
-        if (items.isEmpty()) NeoBlock.LOGGER.warn("Unknown chest ID: '{}'", matcher.group("id"));
+        String name = matcher.group("id");
+        Holder chest = CHESTS.getOrDefault(name, null);
+        if (chest == null) NeoBlock.LOGGER.warn("Unknown chest ID: '{}'", name);
 
         String countString = matcher.group("count");
         int count = (countString != null) ? Integer.parseInt(countString) : 1;
-        return Optional.of(new NeoChestSpec(items, count, matcher.group("id")));
+        return Optional.of(new NeoChestSpec(chest, count, name));
     }
 
-    private final List<NeoItemSpec> items;
+    private final Holder holder;
     private final String id;
 
-    public NeoChestSpec(List<NeoItemSpec> items, int weight, String id) {
+    public NeoChestSpec(Holder holder, int weight, String id) {
         super(Blocks.CHEST, weight);
 
-        this.items = items;
+        this.holder = holder;
         this.id = id;
     }
 
@@ -82,13 +82,20 @@ public class NeoChestSpec extends NeoBlockSpec {
             List<Integer> slots = IntStream.range(0, chest.getContainerSize()).boxed().collect(Collectors.toList());
             Collections.shuffle(slots);
 
-            for (NeoItemSpec item: items) {
-                ItemStack result = item.getStackWithChance();
-                if (result == null) continue;
+            int count = 0;
+            int roll = 0;
+            do {
+                for (NeoItemSpec item : this.holder.items) {
+                    ItemStack result = item.getStackWithChance();
+                    if (result == null || count >= holder.max) continue;
 
-                int slot = slots.remove(0);
-                chest.setItem(slot, result);
-            }
+                    int slot = slots.remove(0);
+                    chest.setItem(slot, result);
+                    count += 1;
+                }
+
+                roll += 1;
+            } while (count < holder.min || roll < 20);
         }
     }
 
@@ -96,10 +103,12 @@ public class NeoChestSpec extends NeoBlockSpec {
         return copy(weight);
     }
     @Override public NeoBlockSpec copy(int weight) {
-        return new NeoChestSpec(items, weight, id);
+        return new NeoChestSpec(holder, weight, id);
     }
 
     @Override public String toString() {
-        return getID() + "{" + items.stream().map(Object::toString).collect(Collectors.joining(";")) + "}";
+        return getID() + "{" + holder.items.stream().map(Object::toString).collect(Collectors.joining(";")) + "}";
     }
+
+    public record Holder(List<NeoItemSpec> items, int min, int max) {}
 }
