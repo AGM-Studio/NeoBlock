d357ac0352189416f1090f01b1f9537a0741bf6f
Ashenguard <myminecraft2019@gmail.com>
2026-09-06 14:22:48 +0330

    JEI is too old, Removed to be added later (NOT PLAYABLE)

--- common/src/main/java/xyz/agmstudio/neoblock/neo/tiers/TierRequirement.java
+++ common/src/main/java/xyz/agmstudio/neoblock/neo/tiers/TierRequirement.java
@@ -1,17 +1,13 @@
 package xyz.agmstudio.neoblock.neo.tiers;
 
-import xyz.agmstudio.neoblock.compatibility.jei.NeoJEIPlugin;
-import xyz.agmstudio.neoblock.neo.world.WorldManager;
-import xyz.agmstudio.neocore.util.StringUtil;
+//import xyz.agmstudio.neoblock.compatibility.jei.NeoJEIPlugin;
 
 import javax.annotation.ParametersAreNonnullByDefault;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 
 @ParametersAreNonnullByDefault
 public interface TierRequirement {
-    void addJEIBox(List<NeoJEIPlugin.TextBox> boxes, AtomicInteger y, TierSpec spec);
+    //void addJEIBox(List<NeoJEIPlugin.TextBox> boxes, AtomicInteger y, TierSpec spec);
     boolean isMet(TierSpec spec);
 
     class BlockBroken implements TierRequirement {
@@ -25,10 +21,10 @@ public interface TierRequirement {
             return spec.block.getBlockCount() >= count;
         }
 
-        @Override public void addJEIBox(List<NeoJEIPlugin.TextBox> boxes, AtomicInteger y, TierSpec spec) {
-            int count = spec.block.getBlockCount();
-            NeoJEIPlugin.addBox(boxes, "jei.neoblock.requirement.blocks_broken", 7, y.getAndAdd(12), this.count <= count, this.count, count);
-        }
+        //@Override public void addJEIBox(List<NeoJEIPlugin.TextBox> boxes, AtomicInteger y, TierSpec spec) {
+        //    int count = spec.block.getBlockCount();
+        //    NeoJEIPlugin.addBox(boxes, "jei.neoblock.requirement.blocks_broken", 7, y.getAndAdd(12), this.count <= count, this.count, count);
+        //}
     }
 
     class GameTime implements TierRequirement {
@@ -43,13 +39,13 @@ public interface TierRequirement {
         }
 
 
-        @Override public void addJEIBox(List<NeoJEIPlugin.TextBox> boxes, AtomicInteger y, TierSpec spec) {
-            long time = WorldManager.getWorldLevel().getGameTime();
-            NeoJEIPlugin.addBox(boxes,
-                    "jei.neoblock.requirement.play_time", 7, y.getAndAdd(12), this.time <= time,
-                    StringUtil.formatTicks(this.time), StringUtil.formatTicks(time)
-            );
-        }
+        //@Override public void addJEIBox(List<NeoJEIPlugin.TextBox> boxes, AtomicInteger y, TierSpec spec) {
+        //    long time = WorldManager.getWorldLevel().getGameTime();
+        //    NeoJEIPlugin.addBox(boxes,
+        //            "jei.neoblock.requirement.play_time", 7, y.getAndAdd(12), this.time <= time,
+        //            StringUtil.formatTicks(this.time), StringUtil.formatTicks(time)
+        //    );
+        //}
     }
 
     class Special implements TierRequirement {
@@ -57,11 +53,11 @@ public interface TierRequirement {
             return spec.commanded;
         }
 
-        @Override public void addJEIBox(List<NeoJEIPlugin.TextBox> boxes, AtomicInteger y, TierSpec spec) {
-            NeoJEIPlugin.addBox(boxes,
-                    spec.commanded ? "jei.neoblock.requirement.command.met" : "jei.neoblock.requirement.command",
-                    7, y.addAndGet(12), spec.commanded
-            );
-        }
+        //@Override public void addJEIBox(List<NeoJEIPlugin.TextBox> boxes, AtomicInteger y, TierSpec spec) {
+        //    NeoJEIPlugin.addBox(boxes,
+        //            spec.commanded ? "jei.neoblock.requirement.command.met" : "jei.neoblock.requirement.command",
+        //            7, y.addAndGet(12), spec.commanded
+        //    );
+        //}
     }
 }
